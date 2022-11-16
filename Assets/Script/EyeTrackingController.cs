bb29944d5aec9221fa93ff2bb52779bfcefed7bb
azisio <axio.mk3@gmail.com>
2022-11-17 00:25:14 +0900

    視線の先のキューブのカラーを変更
    目線Rayの精度ヨシ

--- Assets/Script/EyeTrackingController.cs
+++ Assets/Script/EyeTrackingController.cs
@@ -6,6 +6,10 @@ public class EyeTrackingController : MonoBehaviour
 {
 
     public GameObject lineObj;
+    public GameObject leftEyeObj;
+
+    public Material[] ColorSet = new Material[2];
+
 
     OVREyeGaze eyeGaze;
 
@@ -20,11 +24,30 @@ public class EyeTrackingController : MonoBehaviour
     {
         if (eyeGaze == null)return;
 
-        Debug.Log(eyeGaze.EyeTrackingEnabled);
+        Debug.Log(eyeGaze.TrackingMode);
+        
+        //左目の位置から発射！
+        lineObj.transform.position = leftEyeObj.transform.position;
 
         if (eyeGaze.EyeTrackingEnabled)
         {
-            lineObj.transform.rotation = eyeGaze.transform.rotation;
+            //左目の回転に視線の角度
+            lineObj.transform.rotation = leftEyeObj.transform.rotation * eyeGaze.transform.rotation;
+            
+
+            var leftRay = new Ray(leftEyeObj.transform.position, lineObj.transform.forward);
+            Debug.DrawRay(leftRay.origin, leftRay.direction * 30, Color.red);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(leftRay, out hit)) 
+            {
+                string name = hit.collider.gameObject.name;
+                Debug.Log(name); // コンソールに表示
+
+                hit.collider.gameObject.GetComponent<Renderer>().material = ColorSet[1];
+            }
+
         }
 
     }
